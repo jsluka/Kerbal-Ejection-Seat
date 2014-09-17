51caa907243a200dbd8399b3c02713335378bb6e
justin <sprtn137fl@gmail.com>
2014-09-17 00:35:35 -0400

    Added documentation
    Added a quick link in the main class

--- KerbalEjectionSeat/KES.cs
+++ KerbalEjectionSeat/KES.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
+/// <summary>
+/// This is where I go for tutorials 
+/// http://wiki.kerbalspaceprogram.com/wiki/Plugins
+/// </summary>
+
 public class KES : PartModule
 {
     public override void OnStart(PartModule.StartState state)
