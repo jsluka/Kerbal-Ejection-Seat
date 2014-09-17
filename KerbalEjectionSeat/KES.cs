228a747f4c2703d185ff9ffa9c1b47614408fd08
justin <sprtn137fl@gmail.com>
2014-09-17 00:32:13 -0400

    Initial Commit
    Initial commit of the VS2012 solution with "Hello, Kerbin" test.

---
+++ KerbalEjectionSeat/KES.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class KES : PartModule
+{
+    public override void OnStart(PartModule.StartState state)
+    {
+        print("Hello, Kerbin!");
+    }
+}
